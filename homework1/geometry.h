ca00a24c7b2ce630e939d78e4c1ac7539c665e33
настя <75230747+enestesea@users.noreply.github.com>
2021-03-22 16:33:27 +0300

    Update geometry.h

--- homework1/geometry.h
+++ homework1/geometry.h
@@ -10,117 +10,63 @@ class Point {
 public:
     int x;
     int y;
-    Point() {
-        x = 0;
-        y = 0;
-    }
-    Point(int x_, int y_) {
-        x = x_;
-        y = y_;
-    }
-    Point operator = (const Point& point) {
-        x = point.x;
-        y = point.y;
-        return *this;
-    }
-    Point(const Point& point) {
-        x = point.x;
-        y = point.y;
-    }
+    Point();
+    Point(int, int);
+    Point& operator= (const Point &point); 
+    Point(const Point& point); 
     int getX() const;
     int getY() const;
 };
 
-//todo u dont need inheritance here
-class PolygonalChain : public Point {
+//fixed u dont need inheritance here
+class PolygonalChain {
 public:
-	//todo make destructor
+    //fixed make destructor
     vector <Point> arr;
-    PolygonalChain() {
-        arr.resize(0);
-    }
-    PolygonalChain(const int amount, const Point* other) {
-        for (int i = 0; i < amount; i++) {
-            arr.push_back(other[i]);
-        }
-    }
-    PolygonalChain(const PolygonalChain& other) {
-        arr = other.arr;
-    }
-    PolygonalChain& operator = (const PolygonalChain& other) {
-        arr = other.arr;
-        return *this;
-    }
-    Point getPoint(int i) const {
-        return arr[i];
-    } 
+    PolygonalChain(); 
+    PolygonalChain(const int amount, const Point* other);     
+    PolygonalChain(const PolygonalChain& other); 
+    PolygonalChain& operator = (const PolygonalChain& other);
+    ~PolygonalChain();
+    Point getPoint(int i) const; 
+    float dist(Point p1, Point p2) const;
     virtual int perimeter() const;
     int getN() const;
 };
 
 class ClosedPolygonalChain : public PolygonalChain {
 public:
-    ClosedPolygonalChain() {
-        arr.resize(0);
-    }
+    ClosedPolygonalChain(); 
     int perimeter() const;
-    ClosedPolygonalChain(const int amount, const Point* other) {
-        for (int i = 0; i < amount; i++) {
-            arr.push_back(other[i]);
-        }
-    }
+    ClosedPolygonalChain(const int amount, const Point* other); 
 };
 
 class Polygon : public ClosedPolygonalChain {
 public:
-    Polygon() {
-        arr.resize(0);
-    }
-    Polygon(const int amount, const Point* other) {
-        for (int i = 0; i < amount; i++) {
-            arr.push_back(other[i]);
-        }
-    }
+    Polygon(); 
+    Polygon(const int amount, const Point* other); 
     virtual float area() const;
-
 };
 
 class Triangle : public Polygon {
-
 public:
-    Triangle(const int amount, const Point* other) {
-        for (int i = 0; i < amount; i++) {
-            arr.push_back(other[i]);
-        }
-    }
+    Triangle(const int amount, const Point* other); 
     //fixed without sqrt
     bool hasRightAngle() const;
 };
 
 class Trapezoid : public Polygon {
 public:
-    Trapezoid(const int amount, const Point* other) {
-        for (int i = 0; i < amount; i++) {
-            arr.push_back(other[i]);
-        }
-    }
+    Trapezoid(const int amount, const Point* other); 
     int height() const;
 };
 
 //fixed area and perimeter
 class RegularPolygon : public Polygon {
 public:
-    RegularPolygon() {
-        arr.resize(0);
-    }
-    RegularPolygon(const int amount, const Point* other) {
-        for (int i = 0; i < amount; i++) {
-            arr.push_back(other[i]);
-        }
-    }
+    RegularPolygon(); 
+    RegularPolygon(const int amount, const Point* other); 
     int perimeter() const;
     float area() const;
 };
 #endif // GEOMETRY_H
-
-
