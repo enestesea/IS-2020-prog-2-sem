2dad0a50b4edcf1c466798b92358e387465de6d0
настя <75230747+enestesea@users.noreply.github.com>
2021-04-06 02:18:19 +0300

    Update polynomial.cpp

--- homework2/polynomial.cpp
+++ homework2/polynomial.cpp
@@ -3,82 +3,81 @@
 #include <sstream>
 #include <string>
 #include <cmath>
-#include <limits.h>
 using namespace std;
 
 Polynomial::Polynomial() {
-			max = 0;
-			min = 0;
-			arr = new int[1]{0};
-		};
-Polynomial::Polynomial(int min_, int max_, int*coefficents) {
-			min = min_;
-			max = max_;
-			arr = new int[max_ - min_ + 1];
-			for (int i = 0; i < max - min + 1; i++) {
-				arr[i] = coefficents[i];
-			}
-		};
+	max = 0;
+	min = 0;
+	arr = new int[1]{ 0 };
+};
+Polynomial::Polynomial(int min_, int max_, int* coefficents) {
+	min = min_;
+	max = max_;
+	arr = new int[max_ - min_ + 1];
+	for (int i = 0; i < max - min + 1; i++) {
+		arr[i] = coefficents[i];
+	}
+};
 Polynomial::Polynomial(const Polynomial& copy) {
-			min = copy.min;
-			max = copy.max;
-			arr = new int[max - min + 1];
-			for (int i = 0; i < max - min + 1; i++) {
-				arr[i] = copy.arr[i];
-			}
-		};
+	min = copy.min;
+	max = copy.max;
+	arr = new int[max - min + 1];
+	for (int i = 0; i < max - min + 1; i++) {
+		arr[i] = copy.arr[i];
+	}
+};
 Polynomial::~Polynomial() {
-			delete[]arr;
-		};
-Polynomial& Polynomial::operator =( const Polynomial &copy) {
-			max = copy.max;
-			min = copy.min;
-			arr = new int[max - min + 1];
-			for (int i = 0; i < max - min + 1; i++) {
-				arr[i] = copy.arr[i];
-			}
-			return *this;
-		};
+	delete[]arr;
+};
+Polynomial& Polynomial::operator =(const Polynomial& copy) {
+	max = copy.max;
+	min = copy.min;
+	arr = new int[max - min + 1];
+	for (int i = 0; i < max - min + 1; i++) {
+		arr[i] = copy.arr[i];
+	}
+	return *this;
+};
 
-int Polynomial:: operator [](int i) const {
-			if (i <= max && i >= min)
-				return arr[i - min];
-			else
-				return 0;
-		};
-int& Polynomial:: operator [](int i)  {
-			if (i <= max && i >= min)
-				return arr[i - min];
-			else {
-				int min_ = std::min(min, i);
-				int max_ = std::max(max, i);
-				resize(min_, max_);
-				return arr[i - min];
-			}
-		};
-void Polynomial::resize(int min_, int max_) {
-				int temp[max_ - min_ + 1];
-				for (int j = min_; j <= max_; j++) {
-					if (j >= min && j <= max ) {
-						temp[j - min_] = arr[j - min];
-					}
-					else {
-						temp[j - min_] = 0;
-					}
-				}
-				arr = new int[max_ - min_ + 1];
-				for (int j = 0; j < max_ - min_ + 1; j++) {
-					arr[j] = temp[j];
-				}
-				max = max_;
-				min = min_;
-		};
 float Polynomial::get(int n) {
-			float temp = arr[0] * pow(n, min);
-			float t = temp;
-			for (int i = 1; i < max - min + 1; i++) {
-				temp *= n;
-				t += arr[i] * temp;
-			}
-			return t;
-		};
+	int temp = arr[0] * pow(n, min);
+	int s = temp;
+	for (int i = 1; i < max - min + 1; i++) {
+		temp *= n;
+		s += arr[i] * temp;
+	}
+	return s;
+};
+int Polynomial:: operator [](int i) const {
+	if (i <= max && i >= min)
+		return arr[i - min];
+	else
+		return 0;
+};
+int& Polynomial:: operator [](int i) {
+	if (i <= max && i >= min)
+		return arr[i - min];
+	else {
+		int min_ = std::min(min, i);
+		int max_ = std::max(max, i);
+		resize(min_, max_);
+		return arr[i - min];
+	}
+};
+void Polynomial::resize(int min_, int max_) {
+	int temp[max_ - min_ + 1];
+	for (int j = min_; j <= max_; j++) {
+		if (j >= min && j <= max) {
+			temp[j - min_] = arr[j - min];
+		}
+		else {
+			temp[j - min_] = 0;
+		}
+	}
+	arr = new int[max_ - min_ + 1];
+	for (int j = 0; j < max_ - min_ + 1; j++) {
+		arr[j] = temp[j];
+	}
+	max = max_;
+	min = min_;
+};
