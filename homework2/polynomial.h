d67633cb9bba76c78f655f555f12145ff53492e6
настя <75230747+enestesea@users.noreply.github.com>
2021-04-20 03:44:33 +0300

    Update polynomial.h

--- homework2/polynomial.h
+++ homework2/polynomial.h
@@ -12,49 +12,48 @@ public:
     int* arr;
     Polynomial();
     Polynomial(int min_, int max_, int* coefficents);
-    Polynomial(const Polynomial& copy);
+    Polynomial(const Polynomial &copy);
     ~Polynomial();
-    Polynomial& operator=(const Polynomial& copy);
+    Polynomial& operator=(const Polynomial &copy);
     int operator [](int i) const;
     int& operator [](int i);
     void resize(int min_, int max_);
     float get(int n);
-    friend ostream& operator <<(ostream& stream, const Polynomial& poly);
+    friend ostream& operator <<(ostream& stream, const Polynomial &poly);
 //==
-    friend bool operator==(const Polynomial& poly1, const Polynomial& poly2);
+    friend bool operator==(const Polynomial &poly1, const Polynomial &poly2);
 //!=
-    friend operator !=(const Polynomial& poly1, const Polynomial& poly2);
+    friend operator !=(const Polynomial &poly1, const Polynomial &poly2);
 //+
-    friend Polynomial operator+(const Polynomial& poly);
+    friend Polynomial operator+(const Polynomial &poly);
 //-
-    friend Polynomial operator-(const Polynomial& poly);
+    friend Polynomial operator-(const Polynomial &poly);
 // +=
-    Polynomial operator += (const Polynomial poly2);
+    Polynomial& operator += (const Polynomial& poly2);
+    Polynomial& change (const Polynomial& poly2, int n);
 // -=
-    Polynomial operator -= (const Polynomial poly2);
+    Polynomial& operator -= (const Polynomial& poly2);
 //+
     friend Polynomial operator+(const Polynomial poly1, const Polynomial poly2);
 //-
     friend Polynomial operator-(const Polynomial poly1, const Polynomial poly2);
 //* int*poly
-    friend Polynomial operator *(const Polynomial poly1, const int a);
+    friend Polynomial operator *(const Polynomial &poly1, int a);
 //fixed copy-paste
 //* poly* int
-//todo const&???
-    friend Polynomial operator *(const int a, const Polynomial poly1);
+//fixed const&???
+    friend Polynomial operator *(int a, const Polynomial &poly1);
 //poly*poly
-    friend Polynomial operator *(const Polynomial poly1, const Polynomial poly2);
+    friend Polynomial operator *(const Polynomial &poly1, const Polynomial &poly2);
 //fixed you dont need it
 //*=
-    Polynomial operator*=(const int a);
+    Polynomial& operator*=(int a);
 // /
-    friend Polynomial operator/(Polynomial poly, const int a);
+    friend Polynomial operator/(const Polynomial &poly1, int a);
 //fixed / from /=
 // /=
-    Polynomial operator /=(const int a);
+    Polynomial& operator /=(int a);
 // >>
-    friend istream& operator >>(istream& stream, Polynomial& poly);
+    friend istream& operator >>(istream& stream, Polynomial &poly);
 };
 #endif
-
-
